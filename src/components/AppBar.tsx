1bc564ed67cc73f533bb6e44b4fbdf665125df95
johnfolly <john.folly@cosmotech.com>
2023-12-14 09:55:35 +0100

    imp bar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -8,7 +8,7 @@ interface AppBarProps {
 
 const AppBar: React.FC<AppBarProps> = ({ title }) => {
   return (
-    <nav className="app-bar fixed w-full top-0 left-0 rl-auto pb-6">
+    <nav className="app-bar fixed w-full top-0 left-0 rl-auto pb-6 pr-10">
       <div className="flex mt-12 mx-8">
         <div className="back-button-wrapper">
           <button className="back-button">
@@ -17,7 +17,7 @@ const AppBar: React.FC<AppBarProps> = ({ title }) => {
             </div>
           </button>
         </div>
-        <div className="app-bar-title ml-20 text-p-1">{title}</div>
+        <div className="app-bar-title mx-auto text-p-1">{title}</div>
       </div>
     </nav>
   );

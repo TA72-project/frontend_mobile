d8f91b58cee937d867c0cddc7f75b4647b4b55bc
johnfolly <john.folly@cosmotech.com>
2023-12-15 12:48:28 +0100

    Update styles and schedule display

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -49,7 +49,7 @@ const Schedule = () => {
 		const toFormat = time.toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit", hour12: false });
 		const twoDigitHour = toFormat.split(":")[0];
 		const startTime = Number(twoDigitHour); // Start time in hours
-		const endTime = startTime + 3; // End time in hours
+		const endTime = 18; // End time in hours
 		const slotDuration = 30; // Time slot duration in minutes
 
 		for (let hour = startTime; hour < endTime; hour++) {
@@ -58,7 +58,7 @@ const Schedule = () => {
 				const isCurrentTime = currentTime.getHours() === hour && currentTime.getMinutes() === minute;
 
 				timeSlots.push(
-					<div key={formattedTime} className={`px-2 py-1 mb-4  ${isCurrentTime ? "current-time" : ""}`}>
+					<div key={formattedTime} className={`px-2 py-1  ${isCurrentTime ? "current-time" : ""}`}>
 						{formattedTime}
 					</div>,
 				);
@@ -73,7 +73,7 @@ const Schedule = () => {
 			<div className="grid col row-span-7 grid-rows-7 grid-flow-col">
 				{/* Map over the array of dates and create a button for each date */}
 				{daysArray.map((day, index) => (
-					<button key={index} className={`py-4 px-1 ${index === 3 ? "button-primary-ln" : "btn"} text-p-2`} onClick={() => console.log(`Button clicked for ${index}`)}>
+					<button key={index} className={`row row-span-1 py-4 px-1 ${index === 3 ? "button-primary-ln" : "btn"} text-p-2`} onClick={() => console.log(`Button clicked for ${index}`)}>
 						<div className="col">
 							<div className="row">{formatDateString(day)}</div>
 							<div className="row">{formatWeekDayString(day)}</div>
@@ -83,33 +83,54 @@ const Schedule = () => {
 			</div>
 			<div className="grid grid-rows-1 grid-flow-col-dense justify-items-stretch mt-4">
 				<div className="grid col col-span-1">
-					<div className=" col">{generateTimeSlots(currentTime)}</div>
+					<div className="grid col">{generateTimeSlots(currentTime)}</div>
 				</div>
-				<div className="grid col col-span-12">
+				<div className="grid col">
 					<div className="tickBar"></div>
-					<div className=" shadow-md p-3 rounded-2xl image-canvas  mt-2">
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
 						<div className="grid grid-rows-1 grid-flow-col-dense">
 							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
 						</div>
 						<div className="grid row text-sm-1">11h00 - 11h30</div>
 					</div>
-					<div className=" shadow-md p-3 rounded-2xl image-canvas  mt-2">
-						<div className="grid grid-rows-1 grid-flow-col-dense">
-							<div className="text-p-2 grid col col-span-1 flex">CROCHVILLY (16 Rue des Flammant)</div>
-						</div>
-						<div className="grid row text-sm-1">12h00 - 12h30</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
 					</div>
-					<div className=" shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
 						<div className="grid grid-rows-1 grid-flow-col-dense">
 							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
 						</div>
-						<div className="grid row text-sm-1">12h40 - 13h10</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
 					</div>
-					<div className=" shadow-md p-3 rounded-2xl image-canvas  mt-2">
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
 						<div className="grid grid-rows-1 grid-flow-col-dense">
 							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
 						</div>
-						<div className="grid row text-sm-1">13h10 - 13h40</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
 					</div>
 				</div>
 			</div>
--- src/components/ScheduleDay.tsx
+++ src/components/ScheduleDay.tsx
@@ -49,7 +49,7 @@ const ScheduleDay = () => {
 		const toFormat = time.toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit", hour12: false });
 		const twoDigitHour = toFormat.split(":")[0];
 		const startTime = 6; // Start time in hours
-		const endTime = 22 // End time in hours
+		const endTime = 22; // End time in hours
 		const slotDuration = 30; // Time slot duration in minutes
 
 		for (let hour = startTime; hour < endTime; hour++) {
@@ -58,7 +58,7 @@ const ScheduleDay = () => {
 				const isCurrentTime = currentTime.getHours() === hour && currentTime.getMinutes() === minute;
 
 				timeSlots.push(
-					<div key={formattedTime} className={`px-2 py-1 mb-5  ${isCurrentTime ? "current-time" : ""}`}>
+					<div key={formattedTime} className={`px-2 py-1 mb-12  ${isCurrentTime ? "current-time" : ""}`}>
 						{formattedTime}
 					</div>,
 				);
@@ -73,7 +73,7 @@ const ScheduleDay = () => {
 			<div className="grid col row-span-7 grid-rows-7 grid-flow-col">
 				{/* Map over the array of dates and create a button for each date */}
 				{daysArray.map((day, index) => (
-					<button key={index} className={`py-4 px-1 ${index === 3 ? "button-primary-ln" : "btn"} text-p-2`} onClick={() => console.log(`Button clicked for ${index}`)}>
+					<button key={index} className={`row row-span-1 py-4 px-1 ${index === 3 ? "button-primary-ln" : "btn"} text-p-2`} onClick={() => console.log(`Button clicked for ${index}`)}>
 						<div className="col">
 							<div className="row">{formatDateString(day)}</div>
 							<div className="row">{formatWeekDayString(day)}</div>
@@ -81,25 +81,267 @@ const ScheduleDay = () => {
 					</button>
 				))}
 			</div>
-			<div className="grid grid-rows-1 grid-flow-col-dense justify-items-stretch mt-4">
+			<div className="grid grid-rows-1 grid-flow-col-dense justify-items-stretch mt-4 gap-2">
 				<div className="grid col col-span-1">
-					<div className=" col">{generateTimeSlots(currentTime)}</div>
+					<div className="grid col">{generateTimeSlots(currentTime)}</div>
 				</div>
-				<div className="grid col col-span-12">
-					<div className="tickBar"></div>
-					<div className=" shadow-md  rounded-2xl image-canvas bg-gray-100 mt-2">
-						<div className="my-3 mx-2 flex justify-center  md:justify-end"></div>
+				<div className="grid col">
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
 					</div>
-					<div className=" shadow-md  rounded-2xl image-canvas-2 bg-gray-100 mt-2">
-						<div className="my-3 mx-2 flex justify-center  md:justify-end"></div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
 					</div>
-					<div className=" shadow-md  rounded-2xl image-canvas bg-gray-100 mt-2">
-						<div className="my-3 mx-2 flex justify-center  md:justify-end"></div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
 					</div>
-					<div className=" shadow-md  rounded-2xl image-canvas bg-gray-100 mt-2">
-						<div className="my-3 mx-2 flex justify-center  md:justify-end"></div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
 					</div>
 				</div>
+				<div className="grid col">
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+					<div className="gird row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">FOLLY (6 D Boulevard Anatole)</div>
+						</div>
+						<div className="grid row text-sm-1">11h00 - 11h30</div>
+					</div>
+					<div className="row row-span-1  mt-2">
+						<div className="mt-6 mx-2 h-8 flex justify-center"></div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas-2  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">PERDRIAU (2 Bis rue Thireey Mieg)</div>
+						</div>
+						<div className="grid row text-sm-1">13h00 - 13h30</div>
+					</div>
+					<div className="row row-span-1 shadow-md p-3 rounded-2xl image-canvas  mt-2">
+						<div className="grid grid-rows-1 grid-flow-col-dense">
+							<div className="text-p-2 grid col col-span-1 flex">SIDOL (23 Rue de la Moselle)</div>
+						</div>
+						<div className="grid row text-sm-1">13h30 - 14h00</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	);
